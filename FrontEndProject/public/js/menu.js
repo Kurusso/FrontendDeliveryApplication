a0326d9ca331303b0080bd9868e657ef7e995b7d
Alexey <lesha.braun1029@gmail.com>
2022-11-26 20:40:32 +0700

    fix pagination

--- FrontEndProject/public/js/menu.js
+++ FrontEndProject/public/js/menu.js
@@ -1,8 +1,10 @@
 let isVegetarian;
 var pageJson
+var currentPage=1;
 var pageRegexp= new RegExp("page=([1-9]+[0-9]*)")
 var sortByRegexp= new RegExp("sorting=(NameAsc|NameDesc|PriceAsc|PriceDesc|RatingAsc|RatingDesc)")
 var categoriesRegexp = new RegExp("categories=(WOK|Soup|Pizza|Dessert|Drink)")
+var vegetarianRegexp = new RegExp("vegetarian=(true|false)")
 async function changeLink(){
     let data = document.querySelectorAll(".data-field");
     var selected = [];
@@ -12,39 +14,72 @@ async function changeLink(){
             selected.push(option.value);
         }
     }
-    //https://food-delivery.kreosoft.ru/api/dish?
     let url=`http://localhost:3000/`;
     selected.forEach(element => url=url+`categories=${element}&`);
-    url=url+`vegetarian=${isVegetarian}&sorting=${data[1].value}&page=1`
+    url=url+`vegetarian=${isVegetarian}&sorting=${data[1].value}&page=${currentPage}`
     window.location.href=url;
 
 }
-async function renderPage(){
+async function FetchMenu(urlForFetch){
+    var response = await fetch(urlForFetch);
+    return response.json()
+}
+async function urlConstructor(){
     let url= window.location.href;
-    var pageNumber = pageRegexp.exec(url)[1];
-    var sortBy = sortByRegexp.exec(url)[1];
-    var urlForFetch="https://food-delivery.kreosoft.ru/api/dish?"
-    var categories =[]
-    var m;
+    console.log(url,"url");
+    let pageNumber;
+    let sortBy;
+    let vegetarian;
+    if(pageRegexp.exec(url)!=null){
+        pageNumber=pageRegexp.exec(url)[1];
+    }
+    if(sortByRegexp.exec(url)!=null){
+        sortBy = sortByRegexp.exec(url)[1];
+    }
+    if(vegetarianRegexp.exec(url)!=null){
+        vegetarian = vegetarianRegexp.exec(url)[1];
+    }
+    let urlForFetch="https://food-delivery.kreosoft.ru/api/dish?"
+    let categories =[]
+    let m;
     do {
         m = categoriesRegexp.exec(url);
         if (m) {
-           url= url.slice(m.index+m[0].length)
+            url= url.slice(m.index+m[0].length)
             categories.push(m[1])
         }
     } while (m!=null);
     categories.forEach(element => urlForFetch=urlForFetch+`categories=${element}&`);
-    urlForFetch=urlForFetch+`vegetarian=${isVegetarian}&sorting=${sortBy}&page=1`
-    console.log(urlForFetch)
-    pageJson = (await fetch(urlForFetch));
-    var json=pageJson.json();
+    if(vegetarian!=null) {
+        urlForFetch = urlForFetch + `vegetarian=${vegetarian}`
+    }
+    else {
+        urlForFetch = urlForFetch + `vegetarian=false`
+    }
+    if(vegetarian!=null) {
+        urlForFetch = urlForFetch + `&sorting=${sortBy}`
+    }
+    else {
+        urlForFetch = urlForFetch + `&sorting=NameAsc`
+    }
+    if (pageNumber!=null) {
+        urlForFetch = urlForFetch + `&page=${pageNumber}`
+    }
+    else{
+        urlForFetch = urlForFetch + `&page=1`
+    }
+    return urlForFetch
+}
+async function renderPage(){
+    var urlForFetch= await urlConstructor();
+    pageJson = await FetchMenu(urlForFetch);
    let clone = []
     let i=0;
    document.querySelectorAll(".dish-box").forEach(element=>{
        if(element.style.display=="")
            element.remove();
    })
-   console.log(json)
+   console.log(pageJson)
     pageJson.dishes.forEach(element => {
         clone.push(document.querySelector(".dish-box").cloneNode(true))
         clone[i].style.display="";

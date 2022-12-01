de2b7ebfa185aad30d9c33c2c1c07df65e52a544
Alexey <lesha.braun1029@gmail.com>
2022-12-01 17:20:45 +0700

    test commit

--- FrontEndProject/public/js/cart.js
+++ FrontEndProject/public/js/cart.js
@@ -1,4 +1,19 @@
+
 document.addEventListener("DOMContentLoaded",()=>{
     const footer = document.querySelector(".footer")
     fetch("/html/footer.html").then(res=>res.text()).then(data2=>footer.innerHTML=data2)
+    render();
+    document.querySelectorAll(".input-group").forEach(element=>{
+        element.querySelector(".quantity-left-minus").addEventListener("click",(event)=>{
+            let element = event.target;
+            if(element.parentElement.nextSibling.nextSibling.value>1) {
+                element.parentElement.nextSibling.nextSibling.value--;
+            }
+        })
+        element.querySelector(".quantity-right-plus").addEventListener("click",(event)=>{
+            let element = event.target;
+                element.parentElement.previousSibling.previousSibling.value++;
+        })
+    })
+
 })

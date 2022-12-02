ec08694633251caea2f74077e4ac6d2b52e45b6f
Alexey <lesha.braun1029@gmail.com>
2022-12-02 18:53:18 +0700

    add purchase

--- FrontEndProject/public/js/orders.js
+++ FrontEndProject/public/js/orders.js
@@ -1,4 +1,9 @@
+async function renderOrders(){
+    
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const footer = document.querySelector(".footer")
     fetch("/html/footer.html").then(res => res.text()).then(data2 => footer.innerHTML = data2)
+    renderOrders()
 });
